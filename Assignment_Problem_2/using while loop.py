3413d8368a4b66995d66bd52783396f50ced4d27
paulsandipan5 <100822181+paulsandipan5@users.noreply.github.com>
2022-03-23 16:21:11 +0530

    Update using while loop.py

--- Assignment_Problem_2/using while loop.py
+++ Assignment_Problem_2/using while loop.py
@@ -1,4 +1,4 @@
-#WAP using while loop
+#WAP  to print a dictionary whose keys should be the alphabet from a-z and the value should be corresponding ASCII values
 i = ord('a')
 while i < ord('z')+1:
     dict_ascii[chr(i)] = i

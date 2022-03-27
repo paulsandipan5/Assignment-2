66ea0b3156e61d9ea4c8f3111abf142a44629c79
paulsandipan5 <100822181+paulsandipan5@users.noreply.github.com>
2022-03-27 16:09:32 +0530

    Update with user input list with tuples.py

--- Assignment_Problem_1/with user input list with tuples.py
+++ Assignment_Problem_1/with user input list with tuples.py
@@ -13,3 +13,19 @@ for i in range(len(l)):
         if l[i][1] < l[j][1]:
             l[i],l[j] = l[j],l[i]
 print(l)
+
+
+# or can be done like
+l = []
+n = int(input("Enter the length of list:"))
+for m in range(n):
+    print("Enter the element 1 of tuple",m+1,":")
+    t_1 = int(input())
+    print("Enter the element 2 of tuple",m+1,":")
+    t_2 = int(input())
+    l.append((t_1,t_2)) # no need to write tuple here as writen above just follow the syntan properly
+for i in range(len(l)):
+    for j in range(len(l)):
+        if l[i][1] < l[j][1]:
+            l[i],l[j] = l[j],l[i]
+print(l)
